5257ddc20c414a3e86c4eaf504beedce08c4d41e
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-01-30 19:44:08 -0300

    initial commit

---
+++ catalog_service/src/test/java/com/microsservice/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservice.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

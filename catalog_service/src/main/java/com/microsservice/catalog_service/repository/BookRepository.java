d6a53e48b8416e0a8a3bea4c7de1cdf5b3e52c44
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-01-30 20:24:57 -0300

    Add repository's for Book and Stock

---
+++ catalog_service/src/main/java/com/microsservice/catalog_service/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.microsservice.catalog_service.repository;
+
+import com.microsservice.catalog_service.domain.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+}
---
+++ catalog_service/src/main/java/com/microsservice/catalog_service/repository/StockRepository.java
@@ -0,0 +1,7 @@
+package com.microsservice.catalog_service.repository;
+
+import com.microsservice.catalog_service.domain.StockEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockRepository extends JpaRepository<StockEntity, Long> {
+}

79a209f21527d545b663ed1d901f472ad1db4862
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-02-04 15:52:01 -0300

    Add `existsByIsbn` method to BookRepository for ISBN existence checks

--- catalog_service/src/main/java/com/microsservice/catalog_service/repository/BookRepository.java
+++ catalog_service/src/main/java/com/microsservice/catalog_service/repository/BookRepository.java
@@ -4,4 +4,5 @@ import com.microsservice.catalog_service.domain.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
+    boolean existsByIsbn(String isbn);
 }

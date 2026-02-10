00529a4d24e9754c487bf7e80d536ff4b778a0ef
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-02-10 15:47:35 -0300

    refactor: alter name - BookService for BookCatalogService

--- catalog_service/src/main/java/com/microsservice/catalog_service/controller/BookController.java
+++ catalog_service/src/main/java/com/microsservice/catalog_service/controller/BookController.java
@@ -2,7 +2,7 @@ package com.microsservice.catalog_service.controller;
 
 import com.microsservice.catalog_service.dto.BookRequestDTO;
 import com.microsservice.catalog_service.dto.BookResponseDTO;
-import com.microsservice.catalog_service.service.BookService;
+import com.microsservice.catalog_service.service.BookCatalogService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +15,20 @@ import java.util.List;
 @RequestMapping("/api/v1/books")
 public class BookController {
 
-    private final BookService bookService;
+    private final BookCatalogService bookCatalogService;
 
     @GetMapping("/getBooks")
     public ResponseEntity<List<BookResponseDTO>> getBooks() {
-        return ResponseEntity.ok(bookService.getAllBooks());
+        return ResponseEntity.ok(bookCatalogService.getAllBooks());
     }
 
     @PostMapping("/createBook")
     public ResponseEntity<BookResponseDTO> createBook(@Valid BookRequestDTO bookRequestDTO) {
-        return ResponseEntity.ok(bookService.createBook(bookRequestDTO));
+        return ResponseEntity.ok(bookCatalogService.createBook(bookRequestDTO));
     }
 
     @PutMapping("/updateBook")
     public ResponseEntity<BookResponseDTO> updateBook(@RequestParam("id")  Long id, @Valid BookRequestDTO bookRequestDTO) {
-        return ResponseEntity.ok(bookService.updateBook(id, bookRequestDTO));
+        return ResponseEntity.ok(bookCatalogService.updateBook(id, bookRequestDTO));
     }
 }
--- catalog_service/src/main/java/com/microsservice/catalog_service/service/BookCatalogService.java
+++ catalog_service/src/main/java/com/microsservice/catalog_service/service/BookCatalogService.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class BookService {
+public class BookCatalogService {
 
     private final BookRepository bookRepository;
     private final StockRepository stockRepository;

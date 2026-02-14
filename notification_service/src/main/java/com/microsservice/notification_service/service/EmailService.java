3e329e132dc7c61e2ad8170e50eba7e869efd388
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-02-14 19:17:37 -0300

    feat: add EmailService for sending rental confirmation emails
    - Implemented `EmailService` to send rental confirmation emails with book title and return date.
    - Added logging for successful and failed email sends.

---
+++ notification_service/src/main/java/com/microsservice/notification_service/service/EmailService.java
@@ -0,0 +1,33 @@
+package com.microsservice.notification_service.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendRentalConfirmation(String bookTitle, String email, String returnDate) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email);
+            message.setSubject("Confirmação de Aluguel de Livro");
+            message.setText("Olá,\n\n" +
+                    "Seu aluguel foi confirmado com sucesso!\n\n" +
+                    "Livro: " + bookTitle + "\n" +
+                    "Data de Devolução: " + returnDate + "\n\n" +
+                    "Obrigado por usar nosso serviço!");
+
+            mailSender.send(message);
+            log.info("E-mail enviado com sucesso para: {}", email);
+        } catch (Exception e) {
+            log.error("Erro ao enviar e-mail para {}: {}", email, e.getMessage());
+        }
+    }
+}

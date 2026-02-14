c3e4703deabbb5342b9432403efb44a34be2e933
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-02-14 19:18:47 -0300

    feat: send rental confirmation emails in RentalCreatedListener
    - Integrated `EmailService` with `RentalCreatedListener` to send rental confirmation emails.
    - Added email, book title, and return date to the email content.

--- notification_service/src/main/java/com/microsservice/notification_service/listener/RentalCreatedListener.java
+++ notification_service/src/main/java/com/microsservice/notification_service/listener/RentalCreatedListener.java
@@ -2,8 +2,8 @@ package com.microsservice.notification_service.listener;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import com.microsservice.notification_service.dto.RentalRecivedEventDTO;
+import com.microsservice.notification_service.service.EmailService;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Component;
 public class RentalCreatedListener {
 
     private final ObjectMapper objectMapper;
+    private final EmailService emailService;
 
     @SqsListener("${aws.sqs.livro-alugado-queue}")
     public void listen(String message) {
@@ -26,11 +27,14 @@ public class RentalCreatedListener {
 
             RentalRecivedEventDTO event = objectMapper.readValue(eventMessage, RentalRecivedEventDTO.class);
             log.info("Evento processado: {}", event);
-            bookCatalogService.updateStockOnRental(event.bookId());
+            
+            emailService.sendRentalConfirmation(
+                event.bookTitle(),
+                event.email(),
+                event.returnDate()
+            );
         } catch (Exception e) {
             log.error("Erro ao processar mensagem: {}", e.getMessage());
         }
     }
-
-
 }

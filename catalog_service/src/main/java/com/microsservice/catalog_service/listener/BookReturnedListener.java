a7d8e472993c627ce69a6413a6e4fff1f7744c5e
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-02-14 19:36:11 -0300

    feat: add SQS listener for book returned events in catalog service
    - Implemented `BookReturnedListener` to process messages from the book returned queue.
    - Integrated with `BookCatalogService` to update book stock.

---
+++ catalog_service/src/main/java/com/microsservice/catalog_service/listener/BookReturnedListener.java
@@ -0,0 +1,33 @@
+package com.microsservice.catalog_service.listener;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsservice.catalog_service.dto.BookReturnedReceivedEventDTO;
+import com.microsservice.catalog_service.service.BookCatalogService;
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BookReturnedListener {
+
+    private final BookCatalogService bookCatalogService;
+    private final ObjectMapper objectMapper;
+
+    @SqsListener("${aws.sqs.book-returned-queue}")
+    public void listen(String message) {
+        try {
+            log.info("Mensagem de devolução recebida: {}", message);
+            JsonNode rootNode = objectMapper.readTree(message);
+            String eventMessage = rootNode.get("Message").asText();
+            BookReturnedReceivedEventDTO event = objectMapper.readValue(eventMessage, BookReturnedReceivedEventDTO.class);
+            log.info("Evento de devolução processado: {}", event);
+            bookCatalogService.returnBookToStock(event.bookId());
+        } catch (Exception e) {
+            log.error("Erro ao processar devolução: {}", e.getMessage());
+        }
+    }
+}

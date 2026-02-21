33f58c11be2710ebf71294d6ad1793dbe84f2324
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-02-21 16:03:21 -0300

    feat: load `.env` variables into system properties in NotificationServiceApplication

--- notification_service/src/main/java/com/microsservice/notification_service/NotificationServiceApplication.java
+++ notification_service/src/main/java/com/microsservice/notification_service/NotificationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.microsservice.notification_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 

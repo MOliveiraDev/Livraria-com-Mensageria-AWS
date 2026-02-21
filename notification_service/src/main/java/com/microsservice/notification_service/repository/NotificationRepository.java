bf5da1ab2283177eced31c53545932465130b29d
Oliver.dev <oliveiramaicon16637@gmail.com>
2026-02-21 15:51:49 -0300

    chore: annotate NotificationRepository with @Repository annotation

--- notification_service/src/main/java/com/microsservice/notification_service/repository/NotificationRepository.java
+++ notification_service/src/main/java/com/microsservice/notification_service/repository/NotificationRepository.java
@@ -2,6 +2,8 @@ package com.microsservice.notification_service.repository;
 
 import com.microsservice.notification_service.domain.NotificationEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends MongoRepository<NotificationEntity, String> {
 }
